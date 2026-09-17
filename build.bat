343f5ad8aefd8c74eb5a3e41d48c4d6ea46d4084
Manuel Alburquerque Lopez <manuelalburquerquelopez@gmail.com>
2026-09-17 18:06:39 +0200

    estructura(en principio)

--- build.bat
+++ build.bat
@@ -1,9 +1,24 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Crear la carpeta build si no existe para evitar errores
+if not exist build mkdir build
+
+set "OBJ_FILES="
+
 echo Ensamblando...
-rgbasm -i include/ -o build/main.o src/main.asm
+:: Busca recursivamente todos los archivos .asm en la carpeta src
+for /r src %%i in (*.asm) do (
+    echo Ensamblando: %%i
+    rgbasm -i include/ -o build/%%~ni.o "%%i"
+    
+    :: Guarda la lista de archivos .o generados en una variable
+    set "OBJ_FILES=!OBJ_FILES! build/%%~ni.o"
+)
 
 echo Enlazando...
-rgblink -o build/juego.gb -n build/juego.sym build/main.o
+:: Pasa la variable con todos los .o al linker
+rgblink -o build/juego.gb -n build/juego.sym !OBJ_FILES!
 
 echo Arreglando cabecera...
 rgbfix -v -p 0xFF build/juego.gb
